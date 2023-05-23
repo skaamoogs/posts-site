d90cee416aa9deaa065ff0d65e2cd8812b2d2583
Aleksandr Shabanov <skaamoogs@MacBook-Air-Aleksandr.local>
2023-05-23 22:25:45 +0300

    create post component

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,3 +1,26 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { Post } from "../../components/card/post";
+import { mockPosts } from "../../const";
+import style from "./home.module.scss";
+
 export const Home = () => {
-  return <main>Список постов</main>;
+  return (
+    <main className={style.main}>
+      <Container>
+        <Row xs={1} md={1} lg={2} className={`g-3 ${style.row}`}>
+          {mockPosts.map((post) => (
+            <Col key={post.id}>
+              <Post
+                title={post.title}
+                text={post.body}
+                avatarSrc="./avatar.jpg"
+                id={post.id}
+                size="100px"
+              />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </main>
+  );
 };
